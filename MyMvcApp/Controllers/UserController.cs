aefc75f56d133f9052df92abc4245ee8801238e3
Brooke Morrice <162521502+BrookeMorr@users.noreply.github.com>
2026-05-20 23:29:28 +1200

    Updating UserController

--- MyMvcApp/Controllers/UserController.cs
+++ MyMvcApp/Controllers/UserController.cs
@@ -4,28 +4,28 @@ using MyMvcApp.Models;
 
 namespace MyMvcApp.Controllers;
 
-public class HomeController : Controller
+public class UserController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
+    // Making the form for user
+    [HttpGet]
+    public IActionResult Create()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+    // Handles the form submission
+    [HttpPost]
+    public IActionResult Create(User user)
     {
-        return View();
+        if (!ModelState.IsValid)
+            return View(user);
+        // No datebase, so no persistence currently
+        return RedirectToAction("Success", user);
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    public IActionResult Success(User user)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(user);
     }
 }
+
