79ec6702472d80942746b1cbf231582a92e35d2e
Brooke Morrice <bmor717@aucklanduni.ac.nz>
2026-05-21 23:30:35 +1200

    added lots of css for Create cshtml

--- MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e65b47c381b8f719e44d37e9e7fafcc4671204b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd170611c94ac397186c3dd9c533466031c3f71")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
