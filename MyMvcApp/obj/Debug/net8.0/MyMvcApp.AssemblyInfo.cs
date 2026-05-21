cbe1f3efc4ddcfe3b603007e61438c674086a2bd
Brooke Morrice <bmor717@aucklanduni.ac.nz>
2026-05-21 17:26:42 +1200

    created cshtml file, for the forum to take first and last name

--- MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74f1dba3c7188cb4a24d5150cb06cfb5824314a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e65b47c381b8f719e44d37e9e7fafcc4671204b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
