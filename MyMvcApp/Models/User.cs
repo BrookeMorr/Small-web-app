76fabdaa0ea5a9c109894de173c93db419da0553
Brooke Morrice <bmor717@aucklanduni.ac.nz>
2026-05-21 21:22:08 +1200

    added to FirstName and Lastname to start with empty string to avoid null error

--- MyMvcApp/Models/User.cs
+++ MyMvcApp/Models/User.cs
@@ -2,6 +2,6 @@ namespace MyMvcApp.Models;
 
 public class User {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
 }
