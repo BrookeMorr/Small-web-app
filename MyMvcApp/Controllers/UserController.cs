80b803d9f38d24a5f822aa382e777b516d8f43c3
Brooke Morrice <bmor717@aucklanduni.ac.nz>
2026-05-22 00:14:22 +1200

    Changed POST so that users are saved to a JSON file

--- MyMvcApp/Controllers/UserController.cs
+++ MyMvcApp/Controllers/UserController.cs
@@ -2,11 +2,14 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MyMvcApp.Models;
 
+// Added so it saves user to JSON file
+using System.Text.Json;
+using System.IO;
+
 namespace MyMvcApp.Controllers;
 
 public class UserController : Controller
 {   
-    private static List<User> Users = new List<User>();
 
     // Making the form for user
     [HttpGet]
@@ -17,18 +20,40 @@ public class UserController : Controller
 
     // Handles the form submission
     [HttpPost]
-    public IActionResult Create(User user) {
+    public IActionResult Create(User user)
+    {
     if (!ModelState.IsValid)
         return View(user);
 
-    Users.Add(user);
+    var filePath = Path.Combine(AppContext.BaseDirectory, "users.json");
+
+    List<User> users = new List<User>();
+
+    if (System.IO.File.Exists(filePath))
+    {
+        var existingData = System.IO.File.ReadAllText(filePath);
+
+        if (!string.IsNullOrWhiteSpace(existingData))
+        {
+            users = JsonSerializer.Deserialize<List<User>>(existingData) ?? new List<User>();
+        }
+    }
+
+    users.Add(user);
+
+    var json = JsonSerializer.Serialize(users, new JsonSerializerOptions
+    {
+        WriteIndented = true
+    });
+
+    System.IO.File.WriteAllText(filePath, json);
 
     return RedirectToAction("Success", new
     {
         firstName = user.FirstName,
         lastName = user.LastName
     });
-    }
+}
 
     public IActionResult Success(string firstName, string lastName)
     {
